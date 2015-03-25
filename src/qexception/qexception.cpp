2be25b640d5d36ffe8aa40adf408567b67975244
raandoom <raandoom@users.noreply.github.com>
2015-03-25 23:51:11 +0300

    qexception.cpp code indent

--- src/qexception/qexception.cpp
+++ src/qexception/qexception.cpp
@@ -1,4 +1,4 @@
-    /****************************************************************************
+/****************************************************************************
     **
     ** Copyright (C) 2015 The Qt Company Ltd.
     ** Contact: http://www.qt.io/licensing/
@@ -30,12 +30,12 @@
     ** $QT_END_LICENSE$
     **
     ****************************************************************************/
-    #include "qexception.h"
-    #include "QtCore/qshareddata.h"
-    #ifndef QT_NO_QFUTURE
-    #ifndef QT_NO_EXCEPTIONS
-    QT_BEGIN_NAMESPACE
-    /*!
+#include "qexception.h"
+#include "QtCore/qshareddata.h"
+#ifndef QT_NO_QFUTURE
+#ifndef QT_NO_EXCEPTIONS
+QT_BEGIN_NAMESPACE
+/*!
     \class QException
     \inmodule QtCore
     \brief The QException class provides a base class for exceptions that can transferred across threads.
@@ -58,17 +58,17 @@
     \li QFuture::results()
     \endlist
     */
-    /*!
+/*!
     \fn QException::raise() const
     In your QException subclass, reimplement raise() like this:
     \snippet code/src_corelib_thread_qexception.cpp 2
     */
-    /*!
+/*!
     \fn QException::clone() const
     In your QException subclass, reimplement clone() like this:
     \snippet code/src_corelib_thread_qexception.cpp 3
     */
-    /*!
+/*!
     \class QUnhandledException
     \inmodule QtCore
     \brief The UnhandledException class represents an unhandled exception in a worker thread.
@@ -78,81 +78,81 @@
     on the receiver thread side.
     Inheriting from this class is not supported.
     */
-    /*!
+/*!
     \fn QUnhandledException::raise() const
     \internal
     */
-    /*!
+/*!
     \fn QUnhandledException::clone() const
     \internal
     */
-    void QException::raise() const
-    {
+void QException::raise() const
+{
     QException e = *this;
     throw e;
-    }
-    QException *QException::clone() const
-    {
+}
+QException *QException::clone() const
+{
     return new QException(*this);
-    }
-    void QUnhandledException::raise() const
-    {
+}
+void QUnhandledException::raise() const
+{
     QUnhandledException e = *this;
     throw e;
-    }
-    QUnhandledException *QUnhandledException::clone() const
-    {
+}
+QUnhandledException *QUnhandledException::clone() const
+{
     return new QUnhandledException(*this);
-    }
-    #ifndef Q_QDOC
-    namespace QtPrivate {
-    class Base : public QSharedData
-    {
-    public:
+}
+#ifndef Q_QDOC
+namespace QtPrivate {
+class Base : public QSharedData
+{
+public:
     Base(QException *exception)
-    : exception(exception), hasThrown(false) { }
+        : exception(exception), hasThrown(false) { }
     ~Base() { delete exception; }
     QException *exception;
     bool hasThrown;
-    };
-    ExceptionHolder::ExceptionHolder(QException *exception)
+};
+ExceptionHolder::ExceptionHolder(QException *exception)
     : base(new Base(exception)) {}
-    ExceptionHolder::ExceptionHolder(const ExceptionHolder &other)
+ExceptionHolder::ExceptionHolder(const ExceptionHolder &other)
     : base(other.base)
-    {}
-    void ExceptionHolder::operator=(const ExceptionHolder &other)
-    {
+{}
+void ExceptionHolder::operator=(const ExceptionHolder &other)
+{
     base = other.base;
-    }
-    ExceptionHolder::~ExceptionHolder()
-    {}
-    QException *ExceptionHolder::exception() const
-    {
+}
+ExceptionHolder::~ExceptionHolder()
+{}
+QException *ExceptionHolder::exception() const
+{
     return base->exception;
-    }
-    void ExceptionStore::setException(const QException &e)
-    {
+}
+void ExceptionStore::setException(const QException &e)
+{
     if (hasException() == false)
-    exceptionHolder = ExceptionHolder(e.clone());
-    }
-    bool ExceptionStore::hasException() const
-    {
+        exceptionHolder = ExceptionHolder(e.clone());
+}
+bool ExceptionStore::hasException() const
+{
     return (exceptionHolder.exception() != 0);
-    }
-    ExceptionHolder ExceptionStore::exception()
-    {
+}
+ExceptionHolder ExceptionStore::exception()
+{
     return exceptionHolder;
-    }
-    void ExceptionStore::throwPossibleException()
-    {
+}
+void ExceptionStore::throwPossibleException()
+{
     if (hasException() ) {
-    exceptionHolder.base->hasThrown = true;
-    exceptionHolder.exception()->raise();
+        exceptionHolder.base->hasThrown = true;
+        exceptionHolder.exception()->raise();
     }
-    }
-    bool ExceptionStore::hasThrown() const { return exceptionHolder.base->hasThrown; }
-    } // namespace QtPrivate
-    #endif //Q_QDOC
-    QT_END_NAMESPACE
-    #endif // QT_NO_EXCEPTIONS
-    #endif // QT_NO_QFUTURE
+}
+bool ExceptionStore::hasThrown() const { return exceptionHolder.base->hasThrown; }
+} // namespace QtPrivate
+#endif //Q_QDOC
+QT_END_NAMESPACE
+#endif // QT_NO_EXCEPTIONS
+#endif // QT_NO_QFUTURE
